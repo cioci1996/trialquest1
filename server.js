7dfd3b6b39ae1fa4afeccb64c97d46b528c8d3ce
cioci1996 <alessiokoci1996@gmail.com>
2025-04-14 23:55:41 +0100

    server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
-const poiRoutes = require('./routes/poiRoutes'); // ✅ NEW
-
+const poiRoutes = require('./routes/poiRoutes');
+const shopRoutes = require('./routes/shopRoutes'); 
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,10 +19,10 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 // Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/pois', poiRoutes); // ✅ Use POI route
+app.use('/api/pois', poiRoutes);
+app.use('/api/shop', shopRoutes); 
 
 app.get('/', (req, res) => res.send('TrailQuest Backend is Running!'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
